fe9cae132e7ce6c15f0dad68b0167a1f6b263ed6
raoul <raoul_boulard@hotmail.com>
2015-03-12 16:57:46 +0100

    switch to embeded assets

--- CHANGE.md
+++ CHANGE.md
@@ -1,2 +1,9 @@
 # version 1.0.0
+
+# version 0.0.2
+- remove composer dependency to bower-asset/slideout and use local asset. This is because 
+bower/slideout was not correctly installed in `vendor/bower` (to investigate)
+- include sliedout 0.1.5 (10 Mars 2015)
+
+# version 0.0.1
 - Initial release
--- src/SlideoutAsset.php
+++ src/SlideoutAsset.php
@@ -5,15 +5,16 @@ namespace raoul2000\widget\slideout;
 use yii\web\AssetBundle;
 
 /**
- *
+ * Slideout Assets
+ * 
  * @author Raoul <raoul.boulard@gmail.com>
  */
 class SlideoutAsset extends AssetBundle {
 	
-	public $sourcePath = '@bower/slideout/lib';
+	//public $sourcePath = '@bower/slideout/lib';
 	
 	public function init() {
-		//$this->sourcePath = __DIR__ . '/../assets';
+		$this->sourcePath = __DIR__ . '/../assets';
 		$this->js [] = YII_DEBUG ? 'slideout.js' : 'slideout.min.js';
 	}
 }
